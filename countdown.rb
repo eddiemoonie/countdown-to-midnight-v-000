f1a190e593e4312a8586eb35a31187556706b6db
Eddie Moon <eddiemoonie@gmail.com>
2018-02-13 20:13:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-    puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
@@ -14,8 +14,5 @@ def countdown_with_sleep(number)
     sleep(1)
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
-
-countdown(12)
-countdown_with_sleep(5)
